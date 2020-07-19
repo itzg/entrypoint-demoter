5aa2f355088f277bf71aa29ee4c95eb1c296ef4c
Geoff Bourne <itzgeoff@gmail.com>
2020-07-18 21:30:54 -0500

    Switched to flagsfiller

--- cmd/entrypoint-demoter/main.go
+++ cmd/entrypoint-demoter/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/itzg/entrypoint-demoter"
+	"github.com/itzg/go-flagsfiller"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/exec"
@@ -16,25 +17,27 @@ var (
 	builtBy = ""
 )
 
-var (
-	match       = flag.String("match", "", "Matches the user/group to the owner of the given path")
-	debug       = flag.Bool("debug", false, "Enable debug logging")
-	showVersion = flag.Bool("version", false, "Show version info and exit")
-	stdinOnTerm = flag.String("stdin-on-term", "",
-		"If set, the given content will be written to the sub-command's stdin when TERM signal is received")
-)
+var config struct {
+	Match       string `usage:"Matches the user/group to the owner of the given path"`
+	Debug       bool   `usage:"Enable debug logging"`
+	Version     bool   `usage:"Show version info and exit"`
+	StdinOnTerm string `usage:"If set, the given content will be written to the sub-command's stdin when TERM signal is received"`
+}
 
 func main() {
 
-	flag.Parse()
+	err := flagsfiller.Parse(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	if *showVersion {
+	if config.Version {
 		fmt.Printf("Version=%s, commit=%s, date=%s, builtBy=%s\n",
 			version, commit, date, builtBy)
 		return
 	}
 
-	if *debug {
+	if config.Debug {
 		log.SetLevel(log.DebugLevel)
 	}
 
@@ -43,12 +46,12 @@ func main() {
 		log.Fatal("Requires command and its arguments to execute in demoted state")
 	}
 
-	uid, gid, err := entrypoint_demoter.ResolveIds(*match)
+	uid, gid, err := entrypoint_demoter.ResolveIds(config.Match)
 	if err != nil {
 		log.WithError(err).Fatal("Failed to resolve IDs")
 	}
 
-	err = entrypoint_demoter.RunCommand(uid, gid, *stdinOnTerm, args)
+	err = entrypoint_demoter.RunCommand(uid, gid, config.StdinOnTerm, args)
 	if err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
 			os.Exit(exitErr.ExitCode())
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/itzg/entrypoint-demoter
 
 go 1.14
 
-require github.com/sirupsen/logrus v1.4.2
+require (
+	github.com/itzg/go-flagsfiller v1.4.2
+	github.com/sirupsen/logrus v1.4.2
+)
